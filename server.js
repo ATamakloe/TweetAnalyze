62e49ba34c02bfb2b881f179b6e3f8a0712c6b51
akorli <alberttamakloe@gmail.com>
2019-05-02 13:09:32 -0500

    track package.json

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const Twit = require('twit');
+const nlpWatson = require('ibm-watson/natural-language-understanding/v1.js');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const port = process.env.PORT || 3001;
+require('dotenv').config()
+
+
+const T = new Twit({
+    consumer_key:         process.env.CONSUMER_KEY,
+    consumer_secret:      process.env.CONSUMER_SECRET,
+    access_token:         process.env.ACCESS_TOKEN,
+    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  })
+
+const nlp = new nlpWatson({
+  version: '2018-11-16',
+  iam_apikey: process.env.WATSONKEY;,
+  url: process.env.WATSONURL;
+});
+
+server.listen(port)
+app.use(express.json());
+app.use(express.static(__dirname + '/views'))
+
+
+
+
+
+
+
+async function getTermScore(term="Trump") {
+  let analyzeParams = null;
+  let sentimentscore = null;
+  let results = {};
+  let tweetList = await T.get('search/tweets', {
+    q: term,
+    count: 10
+  }).then(data => data.data.statuses.map(status => status.text)).filter(status => status.includes(term))
+  console.log(tweetList);
+  if (tweetList.length < 1) {
+    results = {"sentimentScore": 0, "tweets": []}
+  } else {
+    analyzeParams = {
+      'text': tweetList.join(". "),
+      'features': {
+        'sentiment': {
+          'document':true,
+        }
+      }
+    }
+    sentimentscore = await nlp.analyze(analyzeParams).then(analysisResults => analysisResults.sentiment.document.score).catch(err => console.log("Error:",err));
+    results = {"sentimentScore":sentimentscore, "tweets":tweetList}
+  }
+
+  return results;
+}
+
+
+io.on('connection', (socket) => {
+  console.log(`${socket.id} has connected`)
+
+let tweetInterval = null;
+  socket.on('term', (data) => {
+    let term = data.term;
+    tweetInterval = setInterval(async () => {
+      let data = await getTermScore(term);
+      socket.volatile.emit('sentdata', data);
+    }, 1000 * 5);
+
+  })
+
+
+
+
+
+
+
+  socket.on('disconnect', (data) => {
+    console.log(`${socket.id} has disconnected`)
+    clearInterval(tweetInterval);
+  })
+});
---
+++ views/index.js
@@ -0,0 +1,93 @@
+let socket = null;
+let term = null;
+let chartlabels = []
+let chartdata = []
+
+var options = {
+  type: 'line',
+  data: {
+    labels: chartlabels,
+    datasets: [
+			{
+				data: chartdata,
+				borderWidth: 1,
+        fill: false,
+        borderColor: 'rgb(53, 146, 213)',
+        backgroundColor:'rgb(53, 146, 213)'
+			}
+		]
+  },
+  options: {
+    legend: {
+      display: false,
+    },
+    responsive: true,
+    maintainAspectRatio:false,
+  	scales: {
+    	yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString:'Score'
+        },
+        ticks: {
+					reverse: false
+        }
+      }],
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString:'Time'
+        },
+        ticks: {
+					reverse: false
+        }
+      }]
+    }
+  }
+}
+
+let ctx = document.getElementById('myChart').getContext('2d');
+let chart = new Chart(ctx, options);
+
+
+function redrawChart() {
+  chartlabels = [];
+  chartdata =[];
+  chart.data.datasets[0].data = chartdata;
+  chart.data.labels = chartlabels;
+}
+
+
+function connectSocket(event) {
+  let time = null;
+  let now = null;
+  term = document.getElementById("searchfield").value
+  event.preventDefault();
+  document.getElementsByClassName("trackedterm").innerHTML = term;
+  if (socket !== null) {
+    socket.disconnect();
+
+    redrawChart();
+
+    socket = io.connect('localhost:3001',{'forceNew':true});
+    socket.emit('term', {term: term});
+
+    socket.on('sentdata', function(data) {
+      time = new Date().toLocaleTimeString();
+      chartlabels.push(time);
+      chartdata.push(data.sentimentScore);
+      chart.update();
+    });
+
+  } else {
+    socket = io.connect('localhost:3001',{'forceNew':true});
+    socket.emit('term', {term: term});
+    socket.on('sentdata', function(data) {
+      time = new Date().toLocaleTimeString();
+      chartlabels.push(time);
+      chartdata.push(data.sentimentScore);
+      chart.update();
+    });
+
+  }
+}
